765452a5262e1d9791312e9001a5a1a623c1de4a
BenLue <BenJule@users.noreply.github.com>
2026-06-25 09:55:28 +0200

    update bambu-studio to 02.07.01.11820-bj.0

--- Casks/bambu-studio.rb
+++ Casks/bambu-studio.rb
@@ -1,8 +1,8 @@
 cask "bambu-studio" do
-  version "02.07.01.11790-bj.0"
-  sha256 "777284c7cc7d4e63116c71e0bf40d33bdba1f8e4ae46baf2772d99fa4946e517"
+  version "02.07.01.11820-bj.0"
+  sha256 "16845e79adcbee8e21c51307a2adfa8ec6ffc68adc77342af67bd3cbd292e89f"
 
-  url "https://github.com/BenJule/BambuStudio/releases/download/v02.07.01.11790-bj.0/BambuStudio_macos-15_universal_V02.07.01.11790.dmg"
+  url "https://github.com/BenJule/BambuStudio/releases/download/v02.07.01.11820-bj.0/BambuStudio_macos-15_universal_V02.07.01.11820.dmg"
   name "BambuStudio"
   desc "Slicer for Bambu Lab 3D printers"
   homepage "https://github.com/BenJule/BambuStudio"
